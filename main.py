7487cffbe12b986c84d52476f713094d62f2ff2a
Gabriel Hygino <62866108+GabrielHygino@users.noreply.github.com>
2023-03-10 03:40:31 -0300

    first version finished

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 from pymongo import MongoClient
 from fastapi import FastAPI
+from requests import get
+from json import loads
 
 app = FastAPI()
-
-
 myclient = MongoClient("mongodb://localhost:27017/")
 mydb = myclient["pokemon"]
 collections = mydb["pokedex"]
 
-var = "bulbasaur"
-
-"""for x in collections.find({"name":f"{var}"}):
-    print (x)"""
-
-
-"""@app.get("/pokelist")
-def pokelist():
-    print(x)"""
-
 @app.get("/pokelist")
 def pokelist(var: str):
     for x in collections.find({"name":f"{var}"}):
-        print (x)
-    return {'Hello ' + var + '!'} 
+        response = get(x["url"])
+        formated_response = loads(response._content.decode())
+    return (formated_response)
